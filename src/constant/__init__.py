2739883362fbbeae1e0ba813e3e0261b1feec75c
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2024-09-11 17:23:50 +0530

    for package creation

---
+++ src/constant/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+
+AWS_SE_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "akashTech"
+MONGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = ""
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENTION = ".pkl"
